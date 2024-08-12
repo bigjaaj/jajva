cc778e4ec23419f3ba6804c46a5e4d46e81913d9
bigjaaj <thomas.simon.2@etu.univ-amu.fr>
2024-08-12 17:01:09 +0200

    modifications

--- jajva.py
+++ jajva.py
@@ -4,21 +4,25 @@ j = sys.argv[1:]
 
 goodpart = lambda x: x[::-1].split(".",1)[1][::-1]
 
+alias = {"prnt":"System.out.println","pub":"public","priv":"private","stat":"static"}
+
 for i in j:
     with open(i) as f:
         with open(i.replace(".jajva",".java"),'w') as g:
-            af = f.read()
-            af = af.replace("prnt","System.out.println")
+            af = f.readlines()
             t = 0
             extra = 1
             if af[0]!="i":
                 g.write(f"public class {goodpart(i)} {{")
             else:
                 g.write(f"public interface {goodpart(i)} {{")
-                t+=2
+                t+=1
             if i == "Main.jajva":
                 g.write("public static void main(String[] args) {")
                 extra+=1
-            g.write(af[t:])
+            for k in af[t:]:
+                for ali in alias:
+                    good = k.replace(ali,alias[ali])
+                g.write(good)
             for _ in range(extra):
                 g.write("}")
---
+++ test.java
@@ -0,0 +1,2 @@
+public class test {public void bob() { System.out.println("bobob...!");}
+public void jaj() {System.out.println("le grand jaaj est inevitable");"}}
