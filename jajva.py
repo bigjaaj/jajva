d6ee2540a95cb5e5b7e82ad5d5c57e0f0028e351
bigjaaj <thomas.simon.2@etu.univ-amu.fr>
2024-08-31 13:50:51 +0200

    Ajout d'alias

--- jajva.py
+++ jajva.py
@@ -4,7 +4,22 @@ j = sys.argv[1:]
 
 goodpart = lambda x: x[::-1].split(".",1)[1][::-1]
 
-alias = {"prnt":"System.out.println","pub":"public","priv":"private","stat":"static"}
+alias = {"prnt":"System.out.println","pub":"public","prv":"private","stat":"static","fin":"final"}
+
+def tokens(j):
+    l = []
+    nl = ""
+    for i in j:
+        if i in ["	"," ",",",";","{","}","(",")","\n",'"']:
+            l.append(nl)
+            nl = ""
+        else:
+            nl = nl+i
+    return l
+
+def readalias(j):
+    return j.replace("_"," ")
+
 
 for i in j:
     with open(i) as f:
@@ -20,9 +35,24 @@ for i in j:
             if i == "Main.jajva":
                 g.write("public static void main(String[] args) {")
                 extra+=1
+            stage = 0
+            mode = 0
+            attributs = []
             for k in af[t:]:
-                for ali in alias:
-                    good = k.replace(ali,alias[ali])
-                g.write(good)
+                if stage==0 and mode == 0:
+                    nwat = []
+                towrite = True
+                good = k
+                mots = tokens(good)
+                print(mots)
+                if mots[0] == "alias":
+                    alias[mots[1]] = readalias(mots[2])
+                    towrite = False
+                    if word in alias:
+                        good = good.replace(word,alias[word])
+                if towrite:
+                    g.write(good)
+                # partie constructeurs
+
             for _ in range(extra):
                 g.write("}")
--- test.java
+++ test.java
@@ -1,2 +1,3 @@
-public class test {public void bob() { System.out.println("bobob...!");}
+public class test {public String jooj;
+public void bob() {System.out.println("bobob...!!");}
 public void jaj() {System.out.println("le grand jaaj est inevitable");"}}
